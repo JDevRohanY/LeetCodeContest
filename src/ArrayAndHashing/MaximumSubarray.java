843f6a958e008354cf5dc577d9eece10f45fa12d
Rohan Yadav <rohanyadavudp10@gmail.com>
2026-05-21 13:43:28 +0530

    53. Maximum Subarray

--- src/ArrayAndHashing/MaximumSubarray.java
+++ src/ArrayAndHashing/MaximumSubarray.java
@@ -4,6 +4,26 @@ package ArrayAndHashing;
 //https://leetcode.com/problems/maximum-subarray/description/
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-
+        //if you encounter -ve sum, move both start and end
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int start = 0;
+        int end = 0;
+        int currSum = 0;
+        while(start<n && end<n){
+            currSum += nums[end];
+            if(currSum >max){
+                max = currSum;
+            }
+            //check curr sum is negative or positive
+            if(currSum<=0){
+                start = end + 1;
+                end = start;
+                currSum = 0;
+            }else{
+                end ++;
+            }
+        }
+        return max;
     }
 }
