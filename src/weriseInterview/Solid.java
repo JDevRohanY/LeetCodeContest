6f0c21b4f37c94936be1cd02dc1a6a201d77cf15
Rohan Yadav <rohanyadavudp10@gmail.com>
2026-05-21 10:30:27 +0530

    //167. Two Sum II - Input Array Is Sorted

---
+++ src/weriseInterview/Solid.java
@@ -0,0 +1,30 @@
+package weriseInterview;
+
+public class Solid {
+    public void createQuery(String query) {
+        //it should not
+        //creating a database connection
+        //creating a query
+        //calling the queru
+    }
+
+    public void createBird(){
+
+    }
+
+    //bird  ->  eat,
+    //flyBird -> fly
+    //nonFlyBird
+    //fly
+    //dance
+
+    //flydance
+    //nonflydance
+    //penguin -> create bird implemented nonflyBird
+
+    //employee [department_id]
+    //department
+
+    //find out all the employees which belongs to engineering department, and joined in last 30 days
+    //
+}
---
+++ src/weriseInterview/findMinElement.java
@@ -0,0 +1,21 @@
+package weriseInterview;
+
+/*
+Input : {2,3,1,3,4,1,9}
+Output : {2,3,0,3,5,0,6}
+
+Sorted array : {1,1,2,3,3,4,9}  - O(n log n)
+Map ->[1->0, 2-> 2, 3-> 3, 4-> 5, 9 -> 6] O(n)
+
+Get the ans : O(n)
+Tc : O(n log n)
+ */
+public class findMinElement {
+    static void main(String[] args) {
+
+    }
+}
+
+//List of employees
+//salary - second lowest
+
