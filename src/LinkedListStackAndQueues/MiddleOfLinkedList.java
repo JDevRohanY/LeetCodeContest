f30e43124d948945ccb5fbdd988d957f6133625c
Rohan Yadav <rohanyadavudp10@gmail.com>
2026-05-22 20:18:04 +0530

    Middle of linked list

---
+++ src/LinkedListStackAndQueues/MiddleOfLinkedList.java
@@ -0,0 +1,16 @@
+package LinkedListStackAndQueues;
+
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
