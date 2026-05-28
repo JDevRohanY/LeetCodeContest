c6c1482e7d475b2265ff0c1a48f0a3d560205a42
Rohan Yadav <rohanyadavudp10@gmail.com>
2026-05-28 19:32:52 +0530

    Max Jump greedy

---
+++ src/Greedy/JumpGame.java
@@ -0,0 +1,13 @@
+package Greedy;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxJump = 0;
+        for(int i=0; i<nums.length; i++){
+            if(i>maxJump) return false;
+            maxJump = Math.max(maxJump, nums[i]);
+        }
+
+        return true;
+    }
+}
