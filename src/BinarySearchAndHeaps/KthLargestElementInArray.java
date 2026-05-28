03dde506dc6fa4e62a57de7213de6c0dd8e92875
Rohan Yadav <rohanyadavudp10@gmail.com>
2026-05-28 17:58:00 +0530

    Kth largest element in array

---
+++ src/BinarySearchAndHeaps/KthLargestElementInArray.java
@@ -0,0 +1,17 @@
+package BinarySearchAndHeaps;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElementInArray {
+    //can you solve it without sorting
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
